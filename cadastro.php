c6fe00b829c2b3a4b46b4683b72072d111cad45e
FelipeXavier99 <felipe.cvxavier@gmail.com>
2023-04-14 17:13:33 -0300

    commit inicial

---
+++ cadastro.php
@@ -0,0 +1,26 @@
+<?php
+
+include("conexao2.php");
+
+$nome=$_POST['nome'];
+$sobrenome=$_POST['sobrenome'];
+$email=$_POST['email'];
+$senha=$_POST['senha'];
+
+$sql="INSERT INTO cadastro(nome,sobrenome,email,senha) 
+VALUES('$nome','$sobrenome','$email','$senha')";
+
+if(mysqli_query($conexao,$sql)){
+	
+	echo " Usuario cadastrado com sucesso";
+	
+}
+
+else {
+	
+	echo "Erro".mysqli_connect_error($conexao);
+	
+	}
+	
+	mysqli_close($conexao);
+?>
---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+include('conexao.php');
+
+if(isset($_POST['email']) || isset($_POST['senha'])) {
+
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu e-mail";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
+
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM cadastro WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) {
+            
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+          //  $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: painel.php");
+
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+
+    }
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+	<div id="form">
+
+    <h3>CONTA</h3>     
+	<br>
+    <form action="" method="POST">
+        <p>
+            <label>E-mail</label>
+            <input type="text" name="email">
+        </p>
+        <p>
+            <label>Senha</label>
+            <input type="password" name="senha">
+        </p>
+        <p>
+            <button type="submit">Entrar</button>
+			<a href="cadastro.html">Cadastrar</a> 
+        </p>
+    </form>
+	</div>
+</body>
+</html>
